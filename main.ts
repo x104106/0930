1a8e94b4ede2f6bb4a40f768c42ab203b0cb6573
x104106 <70998530+x104106@users.noreply.github.com>
2020-09-30 15:05:02 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,16 @@ input.onGesture(Gesture.TiltLeft, function () {
     radio.sendString("L")
     basic.showArrow(ArrowNames.West)
 })
+function 左轉燈 () {
+    bitbot.ledClear()
+    數字 = 0
+    for (let index = 0; index <= 5; index++) {
+        bitbot.setPixelColor(數字 + index, 0x00FF00)
+    }
+}
+input.onGesture(Gesture.ScreenDown, function () {
+    bitbot.rotate(BBRobotDirection.Left, 30)
+})
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(4)
     radio.sendString("number")
@@ -18,19 +28,28 @@ input.onGesture(Gesture.LogoDown, function () {
  */
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "F") {
-        bitbot.goms(BBDirection.Forward, 100, 5000)
+        bitbot.go(BBDirection.Forward, 100)
+        bitbot.ledClear()
+        bitbot.setLedColor(0x0080FF)
     } else if (receivedString == "B") {
-        bitbot.goms(BBDirection.Reverse, 51, 200)
+        bitbot.go(BBDirection.Reverse, 100)
+        bitbot.ledClear()
+        bitbot.setLedColor(0x0080FF)
     } else if (receivedString == "L") {
-        bitbot.rotatems(BBRobotDirection.Left, 60, 200)
+        bitbot.rotate(BBRobotDirection.Left, 60)
+        左轉燈()
+        bitbot.setLedColor(0x8000FF)
     } else if (receivedString == "R") {
-        bitbot.rotatems(BBRobotDirection.Right, 60, 200)
+        bitbot.rotate(BBRobotDirection.Right, 60)
+        右轉燈()
     } else if (receivedString == "number") {
         basic.showNumber(4)
     } else if (receivedString == "S") {
         bitbot.stop(BBStopMode.Coast)
+        bitbot.ledClear()
     } else {
         bitbot.stop(BBStopMode.Coast)
+        bitbot.ledClear()
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -40,6 +59,14 @@ input.onGesture(Gesture.TiltRight, function () {
     radio.sendString("R")
     basic.showArrow(ArrowNames.East)
 })
+function 右轉燈 () {
+    bitbot.ledClear()
+    數字 = 6
+    for (let index = 0; index <= 5; index++) {
+        bitbot.setPixelColor(數字 + index, 0x00FF00)
+    }
+}
+let 數字 = 0
 basic.showNumber(4)
 radio.setGroup(4)
 basic.forever(function () {
