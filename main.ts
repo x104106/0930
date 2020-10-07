60e33d2ebfc078f14b9cc5544f7a1f4abf7d98af
x104106 <70998530+x104106@users.noreply.github.com>
2020-10-07 14:55:33 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,40 +13,61 @@ function 左轉燈 () {
         bitbot.setPixelColor(數字 + index, 0x00FF00)
     }
 }
-input.onGesture(Gesture.ScreenDown, function () {
-    bitbot.rotate(BBRobotDirection.Left, 30)
-})
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(4)
     radio.sendString("number")
 })
+function 前進 () {
+    while (bitbot.sonar(BBPingUnit.Centimeters) > 3) {
+        bitbot.go(BBDirection.Forward, 100)
+        bitbot.setLedColor(0xFF00FF)
+        if (bitbot.sonar(BBPingUnit.Centimeters) <= 3) {
+            bitbot.stop(BBStopMode.Coast)
+            break;
+        }
+    }
+    bitbot.stop(BBStopMode.Coast)
+}
 input.onGesture(Gesture.LogoDown, function () {
     radio.sendString("F")
     basic.showArrow(ArrowNames.North)
 })
-/**
- */
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("D")
+})
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "F") {
-        bitbot.go(BBDirection.Forward, 100)
+        前進()
         bitbot.ledClear()
-        bitbot.setLedColor(0x0080FF)
+        bitbot.setLedColor(0xFF00FF)
     } else if (receivedString == "B") {
         bitbot.go(BBDirection.Reverse, 100)
         bitbot.ledClear()
         bitbot.setLedColor(0x0080FF)
     } else if (receivedString == "L") {
-        bitbot.rotate(BBRobotDirection.Left, 60)
+        bitbot.rotate(BBRobotDirection.Left, 20)
         左轉燈()
         bitbot.setLedColor(0x8000FF)
     } else if (receivedString == "R") {
-        bitbot.rotate(BBRobotDirection.Right, 60)
+        bitbot.rotate(BBRobotDirection.Right, 20)
         右轉燈()
     } else if (receivedString == "number") {
         basic.showNumber(4)
     } else if (receivedString == "S") {
         bitbot.stop(BBStopMode.Coast)
         bitbot.ledClear()
+    } else if (receivedString == "D") {
+        for (let index = 0; index < 100; index++) {
+            bitbot.buzz(true)
+            basic.pause(10)
+            bitbot.buzz(false)
+            basic.pause(10)
+            bitbot.buzz(true)
+            basic.pause(10)
+            bitbot.buzz(false)
+            basic.pause(10)
+        }
+        bitbot.buzz(false)
     } else {
         bitbot.stop(BBStopMode.Coast)
         bitbot.ledClear()
